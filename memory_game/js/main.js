5955f41aaa2f4e25c37d7b6f6265d301071e0917
Atasha Potter <atashapotter@Atashas-MacBook-Pro.local>
2019-03-19 10:05:01 -0400

    created more functions to clean up code and created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,30 +1,25 @@
-console.log("Up and running!");
-
 var cards = ["queen", "queen","king","king"];
 
 var cardsInPlay = [];
 
-//create a variable to represent the first card that the user flipped
-var cardOne = cards[0];
-//add this first card to the cardsInPlay array.
-cardsInPlay.push(cardOne);
-
-console.log("User flipped " + cardOne);
-
-//create a variable to represent the second card that the user flipped
-var cardTwo = cards[2];
-//add this second card to the cardsInPlay array
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
-
-console.log(cardsInPlay);
-
-//checks if 2 cards are flipped AND if they match or not
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again");
+		console.log("Sorry, try again");
 	}
 }
+
+var flipCard = function(cardId) {
+
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+
+if (cardsInPlay.length === 2) {
+	checkForMatch();
+}
+
+}
+
+flipCard(0);
+flipCard(2);
